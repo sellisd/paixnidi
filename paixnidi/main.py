dba95303b6fbca3f154cb8e58d67882408f06ac6
Diamantis Sellis <sellisd@gmail.com>
2025-01-26 23:05:33 +0100

    fix: let user select server

--- paixnidi/main.py
+++ paixnidi/main.py
@@ -15,7 +15,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080", "http://127.0.0.1:8080", "https://sellisd.github.io:8080"],
+    allow_origins=["http://localhost:8080", "http://127.0.0.1:8080", "https://sellisd.github.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
